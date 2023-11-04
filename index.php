298432f202c27051f8f579b2a975d193be5c17d2
Mohamed_Emad <emd9706@gmail.com>
2023-11-04 21:29:55 +0200

    Created Main Files

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Task Manager</title>
+        <link href="CSS/Style.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <div class="title">   
+            <h1>Wellcome Back</h1>
+            
+        </div>
+    </body>
+</html>
